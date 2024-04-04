9cf920ce95a00e3aa5b94cd538d19eeb45a90d0e
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-04-05 01:53:09 +0530

    changethree

--- taskseven/script.js
+++ taskseven/script.js
@@ -1,6 +1,39 @@
-const SearchForm = document.getElementById("search-form")
-const SearchBox = document.getElementById("search-box")
-const SearchResult = document.getElementById("search-result")
-const ShowMoreBtn = document.getElementById("show-more-btn")
+const accessKey = "z-fmBcWHuUqsy9WI8-I7gWEJbQ0QkwffJgJFGAzyAu4";
+const SearchForm = document.getElementById("search-form");
+const SearchBox = document.getElementById("search-box");
+const SearchResult = document.getElementById("search-result");
+const ShowMoreBtn = document.getElementById("show-more-btn");
 
-let keyword
+let keyword = "";
+let page = 1;
+
+async function searchImage()
+{
+    keyword = SearchBox.value;
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results= data.results;
+
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+
+        imageLink.appendChild(image);
+        SearchResult.appendChild(imageLink);
+
+    })
+}
+
+SearchForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+    page = 1;
+    searchImage();
+    
+})
