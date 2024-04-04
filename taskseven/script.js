ec0668723e41f2b4ab3ba11c1305ed074bea08ef
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-04-04 17:47:10 +0530

    changethree

---
+++ taskseven/script.js
@@ -0,0 +1,6 @@
+const SearchForm = document.getElementById("search-form")
+const SearchBox = document.getElementById("search-box")
+const SearchResult = document.getElementById("search-result")
+const ShowMoreBtn = document.getElementById("show-more-btn")
+
+let keyword
