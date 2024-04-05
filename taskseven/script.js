3f6a1b903b9b5f711e69119865a06498314eefed
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-04-05 16:23:14 +0530

    changefour

--- taskseven/script.js
+++ taskseven/script.js
@@ -10,10 +10,14 @@ let page = 1;
 async function searchImage()
 {
     keyword = SearchBox.value;
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
+
+    if(page === 1){
+        SearchResult.innerHTML = "";
+    }
 
     const results= data.results;
 
@@ -29,6 +33,7 @@ async function searchImage()
         SearchResult.appendChild(imageLink);
 
     })
+    ShowMoreBtn.style.display="block";
 }
 
 SearchForm.addEventListener("submit",(e) => {
@@ -37,3 +42,59 @@ SearchForm.addEventListener("submit",(e) => {
     searchImage();
     
 })
+
+ShowMoreBtn.addEventListener("click",()=>{
+    page++;
+    searchImage();
+})
+
+// const accessKey = "z-fmBcWHuUqsy9WI8-I7gWEJbQ0QkwffJgJFGAzyAu4";
+// const SearchForm = document.getElementById("search-form");
+// const SearchBox = document.getElementById("search-box");
+// const SearchResult = document.getElementById("search-result");
+// const ShowMoreBtn = document.getElementById("show-more-btn");
+
+// let keyword = "";
+// let page = 1;
+
+// async function searchImage() {
+//     keyword = SearchBox.value;
+//     const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+
+//     try {
+//         const response = await fetch(url);
+
+//         if (!response.ok) {
+//             throw new Error('Failed to fetch data from Unsplash API: ${response.status} ${response.statusText}');
+//         }
+
+//         const data = await response.json();
+
+//         const results = data.results;
+
+//         if (!results || results.length === 0) {
+//             console.log('No results found');
+//             return;
+//         }
+
+//         results.map((result) => {
+//             const image = document.createElement("img");
+//             image.src = result.urls.small;
+//             const imageLink = document.createElement("a");
+//             imageLink.href = result.links.html;
+//             imageLink.target = "_blank";
+
+//             imageLink.appendChild(image);
+//             SearchResult.appendChild(imageLink);
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         // Handle the error gracefully, display a message to the user, or try again
+//     }
+// }
+// SearchForm.addEventListener("submit",(e) => {
+//         e.preventDefault();
+//         page = 1;
+//         searchImage();
+        
+//     })
